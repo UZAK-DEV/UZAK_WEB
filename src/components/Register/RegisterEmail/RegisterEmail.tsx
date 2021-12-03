73517d383b8113b994c55bc176a0dd2c851b625d
leeminewook <63051680+leeminewook@users.noreply.github.com>
2021-12-03 19:28:29 +0900

    회원가입 이메일 페이지

---
+++ src/components/Register/RegisterEmail/RegisterEmail.tsx
@@ -0,0 +1,43 @@
+import BackgroundCard from "../../common/BackgroundCard";
+import AuthInput from "../../common/Input/AuthInput";
+import "./RegisterEmail.scss";
+import useRegister from "../../../hooks/Register/useRegister";
+import AuthButton from "../../common/Button/AuthButton";
+const RegisterEmail = () => {
+  const { focus, setFocus } = useRegister();
+  return (
+    <>
+      <div className="RegisterEmailForm">
+        <BackgroundCard />
+        <div className="RegisterEmailForm-Options">
+          <div className="RegisterEmailForm-Options-title">회원가입</div>
+          <div className="RegisterEmailForm-Options-inputs">
+            <div className="RegisterEmailForm-Options-inputs-email">
+              <div
+                className={
+                  focus === "email"
+                    ? "RegisterEmailForm-Options-inputs-email-title focus"
+                    : "RegisterEmailForm-Options-inputs-email-title"
+                }
+              >
+                이메일
+              </div>
+              <AuthInput
+                placeholder=""
+                type="text"
+                width="100%"
+                setFocus={setFocus}
+                inputName="email"
+              />
+            </div>
+            <div className="RegisterEmailForm-Options-inputs-submitDiv">
+              <AuthButton text="다음" width="100%" path="/" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RegisterEmail;
---
+++ src/pages/RegisterEmailPage.tsx
@@ -0,0 +1,13 @@
+import SideHeader from "../components/common/SideHeader";
+import RegisterEmail from "../components/Register/RegisterEmail";
+
+const RegisterEmailPage = () => {
+  return (
+    <>
+      <SideHeader />
+      <RegisterEmail />
+    </>
+  );
+};
+
+export default RegisterEmailPage;
